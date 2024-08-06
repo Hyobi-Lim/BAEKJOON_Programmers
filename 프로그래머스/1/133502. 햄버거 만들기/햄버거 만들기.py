61155a8c6076e78e7eac37d89b4b45a604d82b8b
Hyobi Lim <limhb0909@gmail.com>
2024-08-07 01:05:26 +0900

    [level 1] Title: 햄버거 만들기, Time: 1014.36 ms, Memory: 20.2 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.py
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.py
@@ -1,9 +1,3 @@
 def solution(ingredient):
     answer = 0
-    hamburger=''
-    for i in ingredient:
-        hamburger+=str(i)
-        if hamburger[len(hamburger)-4:]=='1231':
-            answer+=1
-            hamburger=hamburger[:len(hamburger)-4]
     return answer
