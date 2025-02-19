e685db5d9f151cde13ef703935f1b0c48ad7aec4
Hyobi Lim <limhb0909@gmail.com>
2025-02-19 23:39:29 +0900

    [Silver V] Title: 단어 나누기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1251. 단어 나누기/단어 나누기.py
@@ -0,0 +1,10 @@
+import sys
+n=sys.stdin.readline().strip()
+rev_dict=set()
+for i in range(len(n)-2,0,-1):
+    for j in range(len(n)-i-1,0,-1):
+        now=n[:i][::-1]+n[i:i+j][::-1]+n[i+j:][::-1]
+        rev_dict.add(now)
+rev_dict=list(rev_dict)
+rev_dict.sort()
+print(rev_dict[0])
