d4a74b786a45374ed05ad4f2c39536787fd25c35
Hyobi Lim <limhb0909@gmail.com>
2025-03-09 15:09:31 +0900

    [Silver II] Title: 격자상의 경로, Time: 44 ms, Memory: 32544 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/10164. 격자상의 경로/격자상의 경로.py
+++ 백준/Silver/10164. 격자상의 경로/격자상의 경로.py
@@ -10,14 +10,14 @@ if k==0:
                 first[i][j]=first[i-1][j]+first[i][j-1]
     print(first[len(first)-1][len(first[0])-1])
 else:
-    first=[[0]*(k%m) for _ in range((k-1)//m+1)]
+    first=[[0]*((k-1)%m+1) for _ in range((k-1)//m+1)]
     for i in range(len(first)):
         for j in range(len(first[i])):
             if i==0 or j==0:
                 first[i][j]=1
             else:
                 first[i][j]=first[i-1][j]+first[i][j-1]
-    second=[[0]*(m-k%m+1) for _ in range(n-((k-1)//m+1)+1)]
+    second=[[0]*(m-((k-1)%m+1)+1) for _ in range(n-((k-1)//m+1)+1)]
     for i in range(len(second)):
         for j in range(len(second[i])):
             if i==0 or j==0:
