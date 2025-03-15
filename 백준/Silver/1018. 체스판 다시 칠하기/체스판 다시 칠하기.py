0b82530314313934ee82c31b280f05365a23b7b8
Hyobi Lim <limhb0909@gmail.com>
2025-03-16 02:48:22 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 84 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -0,0 +1,40 @@
+import sys
+n,m=map(int,sys.stdin.readline().split())
+all=[]
+wb=[
+    ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'],
+    ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'],
+    ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'],
+    ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'],
+    ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'],
+    ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'],
+    ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'],
+    ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W']
+]
+bw=[
+    ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'],
+    ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'],
+    ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'],
+    ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'],
+    ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'],
+    ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'],
+    ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'],
+    ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B']
+]
+for i in range(n):
+    now=list(sys.stdin.readline().strip())
+    all.append(now)
+answer=64
+for i in range(n-7):
+    for j in range(m-7):
+        wbcount=0
+        bwcount=0
+        for a in range(8):
+            for b in range(8):
+                if all[i+a][j+b]!=wb[a][b]:
+                    wbcount+=1
+                if all[i+a][j+b]!=bw[a][b]:
+                    bwcount+=1
+        if answer>min(wbcount,bwcount):
+            answer=min(wbcount,bwcount)
+print(answer)
