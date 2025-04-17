91281ead910b7c92205c042a572013352be1d641
Hyobi Lim <limhb0909@gmail.com>
2025-04-17 11:31:54 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 104 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.py
@@ -0,0 +1,14 @@
+import sys
+n=int(sys.stdin.readline())
+answer=1
+for i in range(1,n//2+1):
+    now=1
+    min_now=min(i,n-2*i)
+    for j in range(min_now):
+        now*=(n-i-j)
+    for j in range(min_now):
+        now//=(j+1)
+    now*=2**i
+    answer+=now
+    answer%=10007
+print(answer)
