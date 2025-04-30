0ba1b097dd596719e4a6099a8b3d692c1710fe06
Hyobi Lim <limhb0909@gmail.com>
2025-04-30 17:18:06 +0900

    [Silver III] Title: N과 M (1), Time: 124 ms, Memory: 37528 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -0,0 +1,7 @@
+import sys
+from itertools import permutations
+n,m=map(int,sys.stdin.readline().split())
+arr=[str(i) for i in range(1,n+1)]
+answer=list(permutations(arr,m))
+for i in answer:
+    print(*i)
